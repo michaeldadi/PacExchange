366df27100e26862192686021179f15ed9f68972
sdabrowski <sd84470n@pace.edu>
2019-05-01 21:25:39 -0400

    Added new activity to display detailed information in a list row after tapping the row. Updated my items for sale fragment to link to new activity as well as added lines between the rows of the recycler view list items

--- app/src/main/java/com/cs639/pacexchange/ItemFragment.java
+++ app/src/main/java/com/cs639/pacexchange/ItemFragment.java
@@ -1,10 +1,12 @@
 package com.cs639.pacexchange;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -19,13 +21,14 @@ public class ItemFragment extends Fragment {
     RecyclerView mRecyclerView;
     Context mContext;
     List<ListData> mListData;
+    MyItemRecyclerViewAdapter mAdapter;
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         mContext = context;
     }
-    //Loadv stuff from database here
+    //Load stuff from database here
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,18 +64,28 @@ public class ItemFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        MyItemRecyclerViewAdapter adapter = new MyItemRecyclerViewAdapter(mContext, mListData, new View.OnClickListener() {
+        mAdapter = new MyItemRecyclerViewAdapter(mContext, mListData, new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 onSellingRowClicked(v);
             }
         });
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false));
-        mRecyclerView.setAdapter(adapter);
+        LinearLayoutManager manager = new LinearLayoutManager(mContext, LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(manager);
+
+        DividerItemDecoration itemDecor = new DividerItemDecoration(mRecyclerView.getContext(), manager.getOrientation());
+        mRecyclerView.addItemDecoration(itemDecor);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     private void onSellingRowClicked(View v)
     {
-
+        Intent intent = new Intent(getActivity(), ItemTradeDetailsActivity.class);
+        int itemPosition = mRecyclerView.getChildAdapterPosition(v);
+        ListData sellerData = mAdapter.getItem(itemPosition);
+        intent.putExtra("SELLER_NAME", sellerData.getSeller());
+        intent.putExtra("SELLER_ITEM", sellerData.getItemType());
+        intent.putExtra("SELLER_ITEM_VALUE", sellerData.getPrice());
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/cs639/pacexchange/ItemTradeDetailsActivity.java
@@ -0,0 +1,49 @@
+package com.cs639.pacexchange;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class ItemTradeDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_trade_details);
+        DisplaySellerData();
+        setButtonClickListeners();
+    }
+    private void DisplaySellerData()
+    {
+        Intent startingIntent = getIntent();
+        TextView sellerNameTextView = findViewById(R.id.seller_name);
+        TextView itemTypeTextView = findViewById(R.id.item_type_name);
+        TextView estimatedValueTextView = findViewById(R.id.estimated_value);
+        sellerNameTextView.setText(startingIntent.getStringExtra("SELLER_NAME"));
+        itemTypeTextView.setText(startingIntent.getStringExtra("SELLER_ITEM"));
+        int sellerItemValue = startingIntent.getIntExtra("SELLER_ITEM_VALUE", 0);
+        estimatedValueTextView.setText(sellerItemValue + "");
+    }
+    private void setButtonClickListeners()
+    {
+        Button backButton = findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        Button proposeTradeButton = findViewById(R.id.propose_trade_button);
+        proposeTradeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+}
+
