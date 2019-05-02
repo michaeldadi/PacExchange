0eab84008e753e1f71a9ff3cd67bfa89e18f5eef
sdabrowski <sd84470n@pace.edu>
2019-05-02 17:25:57 -0400

    Added new activity to display detailed information in a list row after tapping the row. Updated my items for sale fragment to link to new activity as well as added lines between the rows of the recycler view list items

---
+++ app/src/main/java/com/cs639/pacexchange/FirebaseDataManager.java
@@ -0,0 +1,10 @@
+package com.cs639.pacexchange;
+
+public class FirebaseDataManager
+{
+
+    public FirebaseDataManager()
+    {
+
+    }
+}
--- app/src/main/java/com/cs639/pacexchange/ItemTradeDetailsActivity.java
+++ app/src/main/java/com/cs639/pacexchange/ItemTradeDetailsActivity.java
@@ -1,11 +1,14 @@
 package com.cs639.pacexchange;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class ItemTradeDetailsActivity extends AppCompatActivity {
 
